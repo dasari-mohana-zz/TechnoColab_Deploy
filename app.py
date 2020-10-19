bb7d5b35537ffb6d91ffc2de65615bcdd3d3b06a
dasari-mohana <58592783+dasari-mohana@users.noreply.github.com>
2020-10-19 21:56:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 21:54:33 2020
+
+@author: MOHANA D
+"""
+
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('loan.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    output = model.predict(features_value)
+    if(output[0] == 0):
+        return render_template('answer1.html')
+    else:
+        return render_template('answer2.html')
+
+
+if __name__ == "__main__":
+    app.run()
